5111545c0153ec47920853e7aff6f4c74689fbd3
RedEyedMars <greg_estouffey@hotmail.com>
2019-11-05 20:15:32 -0500

    Needed HandleEvent

--- src/events/events.go
+++ src/events/events.go
@@ -32,6 +32,11 @@ func GoFuncEvent(name string, function1 func()) {
 		log.Printf(" <func %s> %s", name, "Finish")
 	}()
 }
+func HandleEvent(event Event) {
+	log.Printf(" <%s> %s", event.GetName(), "Begin")
+	event.Run()
+	log.Printf(" <%s> %s", event.GetName(), "Finish")
+}
 
 func DoneFuncEvent(name string, Function1 func(chan bool), Shutdown chan bool) {
 	log.Printf(" <%s> %s", "func("+name+")", "Done")
