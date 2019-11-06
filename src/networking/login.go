afb44b85625551b5a1e3f1ba961011da090dfe22
RedEyedMars <greg_estouffey@hotmail.com>
2019-11-06 18:51:24 -0500

    Fixed a null pointer exception in login.go on signup.

--- src/networking/login.go
+++ src/networking/login.go
@@ -17,9 +17,9 @@ func setupLoginCommands(registry *ClientRegistry) {
 		hash.Write([]byte(adminPassword))
 		hash.Write(msg)
 		pwdAsString := fmt.Sprintf("%x", hash.Sum(nil)[:])
-		if member := <-databasing.RequestUser("ByPwd", pwdAsString); member != nil {
-			c.name = member.Name
-			c.send <- []byte(fmt.Sprintf("{login_successful:%s}", member.Name))
+		if user := <-databasing.RequestUser("ByPwd", pwdAsString); user != nil {
+			c.name = user.Name
+			c.send <- []byte(fmt.Sprintf("{login_successful:%s}", user.Name))
 			log.Printf(" networking.attempt_login.Login successful")
 		} else {
 			c.send <- []byte("{login_failed}Credentials not accepted, either check your password or your username!")
@@ -29,23 +29,23 @@ func setupLoginCommands(registry *ClientRegistry) {
 	commands["attempt_signup"] = func(c *Client, msg []byte, user []byte) {
 		split := strings.Split(string(msg), ",")
 		username, pwd := split[0], split[1]
-		if member := <-databasing.RequestUser("ByName", username); member != nil {
+		if user := <-databasing.RequestUser("ByName", username); user != nil {
 			c.send <- []byte("{signup_failed}Username taken!")
 		} else {
 
-			log.Printf(" networking.attempt_signup.No member found; good!")
+			log.Printf(" networking.attempt_signup.No user found; good!")
 			hash := sha256.New()
 			hash.Write([]byte(adminPassword))
 			hash.Write([]byte(pwd))
 			pwdAsString := fmt.Sprintf("%x", hash.Sum(nil)[:])
-			if user := <-databasing.RequestUser("ByPwd", pwdAsString); member == nil {
+			if user := <-databasing.RequestUser("ByPwd", pwdAsString); user == nil {
 
 				events.FuncEvent("client.Signup.AddUser", func() {
 					user = <-databasing.InsertUser(username, pwdAsString)
 				})
 				c.name = user.Name
 
-				c.send <- []byte(fmt.Sprintf("{signup_successful:%s}", member.Name))
+				c.send <- []byte(fmt.Sprintf("{signup_successful:%s}", user.Name))
 				log.Printf(" networking.attempt_signup.Signup Successful!")
 			} else {
 				c.send <- []byte("{login_failed}Credentials not accepted, try a different password and username!")
