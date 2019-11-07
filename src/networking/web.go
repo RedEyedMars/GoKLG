53ac1c5894f448f68139373b9b9e4bc14ffcae09
RedEyedMars <greg_estouffey@hotmail.com>
2019-11-06 21:34:30 -0500

    Rename signup to login

--- src/networking/web.go
+++ src/networking/web.go
@@ -110,7 +110,7 @@ func StartWebClient(toClose chan bool) {
 		http.ServeFile(w, r, "src/www/lib/forge-sha256-master/build/forge-sha256.min.js")
 	})
 	handleJs("/login.js", "/messaging.js")
-	handleImgs("/Pending.png", "/Fail.png", "/Success.png", "/Signup.png", "/Signin.png")
+	handleImgs("/Pending.png", "/Fail.png", "/Success.png", "/Signup.png", "/Login.png")
 
 	srv := &http.Server{Addr: ":8080"}
 	events.GoFuncEvent("Networking.ListenAndServe", func() {
